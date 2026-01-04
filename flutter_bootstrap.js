ccb4c7d703b1bff6019d53f3a4de6a64823957ec
hyunwoogod <hyunwoogod@users.noreply.github.com>
2026-01-04 16:02:14 +0000

    deploy: 56657259cfdac868a2999813d02b6c8cf50e3174

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4206146412"
+    serviceWorkerVersion: "2037518338"
   }
 });
